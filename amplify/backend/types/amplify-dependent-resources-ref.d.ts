0e8a05591309c8a682e4d5c3b7598e519f8d6143
Seier <seier.bernal@gmail.com>
2026-05-06 16:12:50 -0600

    Conectando al backend tibaquira

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,6 +3,7 @@ export type AmplifyDependentResourcesAttributes = {
     "educatelabs673e18d9": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
       "UserPoolArn": "string",
